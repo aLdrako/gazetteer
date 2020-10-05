54d2ed7412cafd17479489fb47de254a186a6e2c
Alexandr <7870159+aLdrako@users.noreply.github.com>
2020-10-05 15:12:53 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(window).on('load', function() {
     $('#loading').hide();
 });
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+$("#countrySearchForm").submit(function(e) {
+    e.preventDefault();
+});
