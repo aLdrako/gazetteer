47cace360046568b93dd5e30d6dc9267a5cac0cb
aLdrako <aLdrako@users.noreply.github.com>
2020-10-20 21:35:39 +0100

    Under dev
    Rewritted getting country feature in php, implemented modal

--- php/getCountryFeature.php
+++ php/getCountryFeature.php
@@ -1,23 +1,12 @@
 <?php
 
-	$url='https://alexgo.co.uk/Projects/Gazetteer/php/countries/countries_large.geo.json';
+	$string = file_get_contents('countries/countries_large.geo.json');
 
-    $ch = curl_init();
-    
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL, $url);
+	$array = json_decode($string, true);
 
-	$result = curl_exec($ch);
-
-	curl_close($ch);	
-
-	$decode = json_decode($result, true);	
-
-	$output = $decode['features'];
+	$output = $array['features'];
 	$feature;
 
-	// Optimize with different search type, etc
 	foreach ($output as $key => $value) {
 		if ($_REQUEST['code'] == $value['properties']['ISO_A3']) {
 			$feature = $value;
